b25e1e06134e1542185a337dc7d1c358c35ce5b9
colortear <wdebs91@gmail.com>
2018-04-27 15:29:40 -0700

    fixed test files for compatability

--- test/build_test.sh
+++ test/build_test.sh
@@ -1,33 +1,33 @@
 make -C ../
-echo "#include "'"../include/ft_malloc.h"'"
+echo "#include "'"../include/map_malloc.h"'"
 #include <stdio.h>
 
 int main(void)
 {
 	char **ptr;
 
-	ptr = ft_malloc(sizeof(char *) * 1000);
+	ptr = map_malloc(sizeof(char *) * 1000);
 	for (int i = 0; i < 1000; i++)
 	{
-		ptr[i] = ft_malloc(sizeof(char) * 301);
+		ptr[i] = map_malloc(sizeof(char) * 301);
 		ptr[i][300] = 0;
 		for (int j = 0; j < 300; j++)
 			ptr[i][j] = 'a';
 	}
 	for (int i = 300; i < 400; i++)
-		ft_free(ptr[i]);
+		map_free(ptr[i]);
 	for (int i = 300; i < 400; i++)
 	{
 		if (i % 3)
 		{
-			ptr[i] = ft_malloc(sizeof(char) * 10001);
+			ptr[i] = map_malloc(sizeof(char) * 10001);
 			ptr[i][10000] = 0;
 			for (int j = 0; j < 10000; j++)
 				ptr[i][j] = 'b';
 		}
 		else
 		{
-			ptr[i] = ft_malloc(sizeof(char) * 3001);
+			ptr[i] = map_malloc(sizeof(char) * 3001);
 			ptr[i][3000] = 0;
 			for (int j = 0; j < 3000; j++)
 				ptr[i][j] = 'c';
@@ -35,13 +35,13 @@ int main(void)
 	}
 	show_alloc_mem();
 	for (int i = 0; i < 1000; i++)
-		ft_free(ptr[i]);
-	ft_free(ptr);
+		map_free(ptr[i]);
+	map_free(ptr);
 	show_alloc_mem();
 	return (0);
 }" > test.c
 cd ../
-gcc -Wall -Wextra -Werror -g -I../include -L. ./test/test.c -o test_tmp -lft_malloc_$(uname -m)_$(uname -s)
+gcc -Wall -Wextra -Werror -g -I../include -L. ./test/test.c -o test_tmp -lmap_malloc_$(uname -m)_$(uname -s)
 export LD_LIBRARY_PATH=$PWD
 mv test_tmp ./test
 cd ./test
--- test/rebuild.sh
+++ test/rebuild.sh
@@ -1,6 +1,6 @@
 make -C ../ re
 cd ..
-gcc -Wall -Werror -Wextra -g -I../include -L. ./test/test.c -o test_tmp -lft_malloc_$(uname -m)_$(uname -s)
+gcc -Wall -Werror -Wextra -g -I../include -L. ./test/test.c -o test_tmp -lmap_malloc_$(uname -m)_$(uname -s)
 mv test_tmp ./test
 cd ./test
 mv test_tmp test
